42d3160d3c21f3962c6a402385b7501b38398f72
Wee Jian Hui Kevin <kevin@Wees-MacBook-Air.local>
2024-01-02 16:19:41 +0800

    feat: Complete styling, add data attribute to each book

--- src/script.js
+++ src/script.js
@@ -1,22 +1,22 @@
 const myLibrary = [];
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages) {
     this.title = title,
     this.author = author,
     this.pages = pages, 
-    this.isRead = isRead,
+    this.isRead = false, // Toggle to true below
     this.info = () => {
-        return `${title} by ${author}, ${pages} pages, ${isRead ? 'done reading' : 'not read yet'}`;
+        return `${title} by ${author}, ${pages} pages.`;
     }
 }
 
-function addBookToLibrary() {
+const addBookToLibrary = (e) => {
+    e.preventDefault();
     const newTitle = window.prompt("What is the title of the book you would like to add to the library?");
     const newAuthor = window.prompt("Who wrote this book?");
     const newPages = parseInt(window.prompt("How many pages does this book have?"));
-    const newIsRead = window.prompt("Have you completed reading this book?") == 'yes' ? true : false;
     
-    const newBook = new Book(newTitle, newAuthor, newPages, newIsRead);
+    const newBook = new Book(newTitle, newAuthor, newPages);
 
     myLibrary.push(newBook);
     updateBookShelf();
@@ -24,13 +24,32 @@ function addBookToLibrary() {
 }
 
 const addBtn = document.getElementById("add");
-window.addEventListener('click', addBookToLibrary)
+addBtn.addEventListener("click", addBookToLibrary);
 
 const bookShelf = document.getElementById("bookShelf");
 const updateBookShelf = () => {
+    bookShelf.innerHTML = '';
+    let bookCount;
+    
     myLibrary.forEach((book) => {
-        console.log(book);
-        bookShelf.innerHTML += `<div class='border rounded-md'>${book.title}</div>`;
+        const bookBlock = document.createElement('div');
+        bookBlock.innerHTML = `<h3 class='font-semibold text-center'>${book.title}</h3><span class='text-center'> by ${book.author}</span>`;
+        bookBlock.classList.add("h-28","w-20","border-3","border-green-400","bg-gray-100","bg-opacity-80","text-blue-800","rounded-md","items-center","justify-center","flex","flex-col");
+        bookBlock.setAttribute("id", "book");
+        bookCount = document.querySelectorAll("#book").length;
+        bookBlock.setAttribute("data-book-count", bookCount);
+        // console.log(bookCount);
+        bookShelf.append(bookBlock);
+        
+        // bookShelf.innerHTML += `<div id='book' class='h-28 w-20 border-3 border-green-400 bg-gray-100 bg-opacity-80 text-blue-800 rounded-md items-center justify-center flex flex-col' data-isRead=${book.isRead}></div>`;
+        // if (book.getAttribute("data-isread") == 'false') {
+        //     console.log(book);
+        // }
+    })
+
+    const books = document.querySelectorAll('#book');
+    books.forEach((book) => {
+        
     })
 }
 
